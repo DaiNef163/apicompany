6f9eb61db5baad0554445b11f4a4811de15de15c
hoainam <hoainambld2003@gmail.com>
2024-04-03 02:19:05 +0700

    view users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,24 @@
 import { Router } from "express";
-import { createUser, getUsers, getUser } from "../controllers/user.controller.js";
+import {
+  createUser,
+  getUsers,
+  getUser,
+  displayallUser,
+} from "../controllers/user.controller.js";
 import { isAdmin, verifyToken } from "../middlewares/authJwt.js";
 import { checkExistingUser } from "../middlewares/verifySignup.js";
-import { createEmployee, getEmployees, getEmployee } from "../controllers/employee.controller.js";
+import {
+  createEmployee,
+  getEmployees,
+  getEmployee,
+} from "../controllers/employee.controller.js";
 const router = Router();
 
 // router.post("/", [verifyToken, isAdmin, checkExistingUser], createUser);
 // router.get("/", [verifyToken, isAdmin, checkExistingUser], getUsers);
 router.post("/", [checkExistingUser], createUser);
 router.get("/", getUsers);
+router.get("/displayUsers", displayallUser);
 router.get("/:userId", [verifyToken, isAdmin, checkExistingUser], getUser);
 
 export default router;
